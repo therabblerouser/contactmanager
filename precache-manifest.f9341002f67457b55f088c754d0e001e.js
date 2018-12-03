54d230ca76e0fc09bb881e6179273d2fb4859b2b
therabblerouser <mccannjoe42@gmail.com>
2018-12-02 22:54:37 -0600

    Updates

---
+++ precache-manifest.f9341002f67457b55f088c754d0e001e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6188adc52db59340d97a",
+    "url": "/static/js/main.6188adc5.chunk.js"
+  },
+  {
+    "revision": "344c191aea24409e3f0a",
+    "url": "/static/js/1.344c191a.chunk.js"
+  },
+  {
+    "revision": "6188adc52db59340d97a",
+    "url": "/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "344c191aea24409e3f0a",
+    "url": "/static/css/1.9c9e4b50.chunk.css"
+  },
+  {
+    "revision": "f0f625400143d146b54913848eac018d",
+    "url": "/index.html"
+  }
+];
